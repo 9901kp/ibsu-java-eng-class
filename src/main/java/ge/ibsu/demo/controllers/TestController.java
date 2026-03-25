f06c409135a80cfafe3399e630cf01c11d1d8664
Kaipov Mukhammed Marsovich <117507679+9901kp@users.noreply.github.com>
2026-03-25 10:47:40 +0400

    init-project

---
+++ src/main/java/ge/ibsu/demo/controllers/TestController.java
@@ -0,0 +1,27 @@
+package ge.ibsu.demo.controllers;
+import ge.ibsu.demo.dto.TestPersonInfo;
+import org.springframework.web.bind.annotation.*;
+
+import ge.ibsu.demo.dto.TestPerson;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "Hello From Server";
+    }
+
+    @PostMapping("/call")
+    public TestPerson call(@RequestBody TestPerson p) {
+        p.setFirstName(p.getFirstName() + " from server");
+        return p;
+    }
+
+    @PostMapping("/testRecords")
+    public TestPersonInfo testCallRecord(@RequestBody TestPersonInfo p) {
+        System.out.println(p.firstName() + " " + p.lastName() + " " + p.age());
+        return p.withAge(30);
+    }
+}
---
+++ src/main/java/ge/ibsu/demo/dto/TestPersonInfo.java
@@ -0,0 +1,7 @@
+package ge.ibsu.demo.dto;
+
+public record TestPersonInfo(String firstName, String lastName, Integer age){
+    public TestPersonInfo withAge(Integer age) {
+        return new TestPersonInfo(this.firstName, this.lastName, this.age);
+    }
+}
